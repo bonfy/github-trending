bb6cee23998197869d44650b3def9535a40336cf
bonfy <foreverbonfy@163.com>
2017-02-08 20:11:01 +0800

    add go to scraper

--- scraper.py
+++ scraper.py
@@ -29,14 +29,14 @@ def scrape(language, filename):
         'User-Agent'		: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:11.0) Gecko/20100101 Firefox/11.0', 'Accept'			: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8', 'Accept-Encoding'	: 'gzip,deflate,sdch', 'Accept-Language'	: 'zh-CN,zh;q=0.8'
     }
 
-    url = 'https://github.com/trending?l={language}'.format(language=language)
+    url = 'https://github.com/trending/{language}'.format(language=language)
     r = requests.get(url, headers=HEADERS)
     assert r.status_code == 200
 
-    print r.encoding
+    # print(r.encoding)
 
     d = pq(r.content)
-    items = d('li.repo-list-item')
+    items = d('ol.repo-list li')
 
     # codecs to solve the problem utf-8 codec like chinese
     with codecs.open(filename, "a", "utf-8") as f:
@@ -46,14 +46,12 @@ def scrape(language, filename):
             i = pq(item)
             title = i("h3 a").text()
             owner = i("span.prefix").text()
-            description = i("p.repo-list-description").text()
+            description = i("p.col-9").text()
             url = i("h3 a").attr("href")
             url = "https://github.com" + url
-            ownerImg = i("p.repo-list-meta a img").attr("src")
-
-            print title, owner, description, url, ownerImg
-            f.write(u"* <img src='{ownerImg}' height='20' width='20'>[{title}]({url}):{description}\n".format(
-                ownerImg=ownerImg, title=title, url=url, description=description))
+            # ownerImg = i("p.repo-list-meta a img").attr("src")
+            # print(ownerImg)
+            f.write(u"* [{title}]({url}):{description}\n".format(title=title, url=url, description=description))
 
 
 def job():
@@ -64,10 +62,11 @@ def job():
     # create markdown file
     createMarkdown(strdate, filename)
 
-    # TODO: use goroutinez
+    # write markdown
     scrape('python', filename)
     scrape('swift', filename)
     scrape('javascript', filename)
+    scrape('go', filename)
 
     # git add commit push
     git_add_commit_push(strdate, filename)
